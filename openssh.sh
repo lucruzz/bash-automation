6bfb323abe56800b75fe37434d8ebe475a3f8c16
Lucas Cruz <62730384+lucruzz@users.noreply.github.com>
2023-02-10 10:02:01 -0300

    Create openssh.sh

---
+++ openssh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+yum install https://yum.oracle.com/repo/OracleLinux/OL8/baseos/latest/x86_64/getPackage/openssh-clients-8.0p1-13.el8.x86_64.rpm -y
+yum install https://yum.oracle.com/repo/OracleLinux/OL8/baseos/latest/x86_64/getPackage/openssh-server-8.0p1-13.el8.x86_64.rpm -y
+
